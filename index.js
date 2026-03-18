b37ce29ddb86b964af725fe7e3b73b98152401ca
zonestore44552-web <zonestore44552@gmail.com>
2026-03-17 22:16:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -91,3 +91,13 @@ async function listInfinitePayEmails() {
         console.error('Erro ao acessar Gmail:', error);
     }
 }
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Rodando');
+});
+
+app.listen(8080, () => {
+    console.log('Servidor rodando na porta 8080');
+});
